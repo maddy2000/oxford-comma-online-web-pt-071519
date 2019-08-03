0520e8e33ed099009b629b9406c48ed865a73b9c
Madelyn Deshield <madelynwallop@gmail.com>
2019-08-03 17:12:30 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,21 @@
 def oxford_comma(array)
+ if array.count ==1
+ array[0]
+ elsif array.count ==2
+ array.join (" and ")
+ else 
+   last_word = array.pop
+   comma_string = array.join (", ")
+   return comma_string + ", and " + last_word
+end 
+ 
+ 
+ 
+ 
+ end
+ 
 
-end
+ 
+
+
+ 
